c05b8d7535e03cf69243c01d72062af4d035ea70
Alvaro Garcia <derecerca@gmail.com>
2016-01-26 13:18:50 +0100

    create project

---
+++ project.clj
@@ -0,0 +1,5 @@
+(defproject csv-merger "0.0.1-SNAPSHOT"
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                           [org.clojure/data.csv "0.1.3"]]
+  :profiles
+    {:uberjar {:aot :all}})
